beef57581d1f458734a3e4650b2c5033c3622ed8
Andrew Lee <alee14498@protonmail.com>
2020-07-20 22:59:08 -0400

    GPL stuff

--- bot.js
+++ bot.js
@@ -1,11 +1,21 @@
 /*********************************************
  * 
- *  DLMP3 Bot
- *  Programmed by Andrew Lee 
- * 
+ *  DLMP3 Bot: A Discord bot that plays local mp3 audio tracks.
  *  (C) Copyright 2020
- *  Licensed with GPL-3.0
- * 
+ *  Programmed by Andrew Lee 
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *********************************************/
 const Discord = require('discord.js');
 const fs = require('fs');
